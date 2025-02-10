1a13849c2a658c4d457d44d6bdfd9199989ff265
lakshman_reddy <147997328+lakshman7781@users.noreply.github.com>
2025-02-10 11:27:15 +0530

    remove ScrollProgress component and its demo; add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: [
+        'next/core-web-vitals', // Use Next.js recommended settings
+    ],
+    rules: {
+        '@typescript-eslint/no-empty-object-type': 'off', // Disable the empty object type rule
+    },
+};
